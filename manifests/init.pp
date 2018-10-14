6cf31ae9d1142b64a660e1a8312fac45f342391c
Boris Peterbarg <boris@seekingalpha.com>
2018-10-14 15:14:43 +0300

    Add UPM control
    UPM chokes RC at higher user counts. Limit which instances will enable
    it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,22 +13,23 @@
 # Copyright 2017 Karol Kozakowski
 #
 class rocketchat (
-  $port           = $rocketchat::params::port,
-  $instance_count = $rocketchat::params::instance_count,
-  $root_url       = $rocketchat::params::root_url,
-  $download_path  = $rocketchat::params::download_path,
-  $destination    = $rocketchat::params::destination,
-  $package_ensure = $rocketchat::params::package_ensure,
-  $package_source = $rocketchat::params::package_source,
-  $mongo_host     = $rocketchat::params::mongo_host,
-  $database_name  = $rocketchat::params::database_name,
-  $mongo_port     = $rocketchat::params::mongo_port,
-  $mongo_replset  = $rocketchat::params::mongo_replset,
-  $authsource     = $rocketchat::params::authsource,
-  $nodejs_version = $rocketchat::params::nodejs_version,
-  $nodejs_deps    = $rocketchat::params::nodejs_deps,
-  $manage_repos   = $rocketchat::params::manage_repos,
-  $verbose        = $rocketchat::params::verbose,
+  $port                  = $rocketchat::params::port,
+  $instance_count        = $rocketchat::params::instance_count,
+  $root_url              = $rocketchat::params::root_url,
+  $download_path         = $rocketchat::params::download_path,
+  $destination           = $rocketchat::params::destination,
+  $package_ensure        = $rocketchat::params::package_ensure,
+  $package_source        = $rocketchat::params::package_source,
+  $mongo_host            = $rocketchat::params::mongo_host,
+  $database_name         = $rocketchat::params::database_name,
+  $mongo_port            = $rocketchat::params::mongo_port,
+  $mongo_replset         = $rocketchat::params::mongo_replset,
+  $authsource            = $rocketchat::params::authsource,
+  $nodejs_version        = $rocketchat::params::nodejs_version,
+  $nodejs_deps           = $rocketchat::params::nodejs_deps,
+  $manage_repos          = $rocketchat::params::manage_repos,
+  $verbose               = $rocketchat::params::verbose,
+  $user_presence_monitor = $rocketchat::params::user_presence_monitor,
 ) inherits rocketchat::params {
 
   class { 'rocketchat::packages':
@@ -54,15 +55,16 @@ class rocketchat (
   }
 
   class { 'rocketchat::service':
-    port           => $port,
-    instance_count => $instance_count,
-    mongo_host     => $mongo_host,
-    mongo_port     => $mongo_port,
-    mongo_replset  => $mongo_replset,
-    authsource     => $authsource,
-    database_name  => $database_name,
-    root_url       => $root_url,
-    destination    => $destination,
-    require        => Class['rocketchat::install']
+    port                  => $port,
+    instance_count        => $instance_count,
+    mongo_host            => $mongo_host,
+    mongo_port            => $mongo_port,
+    mongo_replset         => $mongo_replset,
+    authsource            => $authsource,
+    database_name         => $database_name,
+    root_url              => $root_url,
+    user_presence_monitor => $user_presence_monitor,
+    destination           => $destination,
+    require               => Class['rocketchat::install']
   }
 }
